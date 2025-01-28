9fc6aeeac5253b90faa74ade9b0245c06d22a195
FOWMind <alwl1@hotmail.com>
2025-01-28 05:20:04 -0300

    feat(landing): favicon (#8)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          type="image/x-icon"
+          sizes="32x32"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
