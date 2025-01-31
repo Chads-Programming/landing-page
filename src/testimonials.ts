93f838a15b58df2f7318bcfae644d8be9c3a8c01
Andres Velasco <velasco.andrs@gmail.com>
2025-01-31 00:02:55 -0500

    feat: change order

--- src/testimonials.ts
+++ src/testimonials.ts
@@ -1,11 +1,5 @@
 export default [
   {
-    text: 'I found my first dev job through connections made in this community. The mentorship program is fantastic!',
-    author: 'Pablo Bartolomé Gallardo',
-    role: 'Software Developer',
-    image: 'https://avatars.githubusercontent.com/u/121761009?v=4',
-  },
-  {
     text: ' The collaborative projects helped me build a strong portfolio. This community is a game-changer for developers.',
     author: 'Daniele Sileoni',
     role: 'Freelance developer',
@@ -19,6 +13,18 @@ export default [
       'https://media.licdn.com/dms/image/v2/D4D03AQHYugQuEeCt1g/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1724083002388?e=1743638400&v=beta&t=f1KothKGMKM1CLWHel016ovDOmkGCdzAXyD0yFYdVhU',
   },
   {
+    text: `Joining to this community, I have connected with amazing friends who guided me, helped improve my projects, and made me a better programmer. I wouldn't be where I am without them!`,
+    author: 'Ian Espinillo',
+    role: 'Software engineering student',
+    image: '/ian.jpg',
+  },
+  {
+    text: 'I found my first dev job through connections made in this community. The mentorship program is fantastic!',
+    author: 'Pablo Bartolomé Gallardo',
+    role: 'Software Developer',
+    image: 'https://avatars.githubusercontent.com/u/121761009?v=4',
+  },
+  {
     text: 'Thanks to chads programming, I built a network, gained confidence, and landed my first programming job through a connection. Community is everything!',
     author: 'Agustín Piriz',
     role: 'Frontend developer',
@@ -29,11 +35,5 @@ export default [
     author: 'Marcial Fantini',
     role: 'Software developer',
     image: '/marchu-dev.webp',
-  },
-  {
-    text: `Joining to this community, I have connected with amazing friends who guided me, helped improve my projects, and made me a better programmer. I wouldn't be where I am without them!`,
-    author: 'Ian Espinillo',
-    role: 'Software engineering student',
-    image: '/ian.jpg',
   },
 ]
