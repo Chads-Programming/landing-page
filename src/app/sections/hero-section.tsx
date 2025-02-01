6bff7e08377e74baf6d487743661d56c1d119aee
Andres Velasco <velasco.andrs@gmail.com>
2025-01-31 23:57:47 -0500

    feat: added discord icon

--- src/app/sections/hero-section.tsx
+++ src/app/sections/hero-section.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { TextAnimatedGradient } from '@/components/text-animated'
+import { Discord } from '@/icons/discord'
 import { motion } from 'framer-motion'
 import { Users } from 'lucide-react'
 import Image from 'next/image'
@@ -51,8 +52,8 @@ export const HeroSection = () => {
                 {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
                 <button className="text-lg relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50">
                   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gray-950 px-8 py-1 font-medium text-gray-50 backdrop-blur-3xl">
-                    Join Discord
+                  <span className="inline-flex h-full gap-4 w-full cursor-pointer items-center justify-center rounded-full bg-gray-950 px-8 py-1 font-medium text-gray-50 backdrop-blur-3xl">
+                    <Discord className="w-6 h-6" /> Join Discord
                   </span>
                 </button>
               </Link>
